1f688e138c1b297cd1bb6737467e24cd21f55b46
1101137148 <1101137148@gm.kuas.edu.tw>
2015-12-17 15:44:42 +0800

    password use base64

--- db.inc.php
+++ db.inc.php
@@ -3,8 +3,8 @@ function init_db() {
     $db_host = 'db.mis.kuas.edu.tw';
     $db_name = 's1101137114';
     $db_user = 's1101137114';
-    $password = 'aGF0ZWRi';
-    $db_password =  base64_decodecode($password);
+    $tmp = 'aGF0ZWRi';
+    $db_password =  base64_decodecode($tmp);
     $db_dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
     $db = new PDO($db_dsn, $db_user, $db_password);
     return $db;
